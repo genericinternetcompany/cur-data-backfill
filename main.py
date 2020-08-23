3f7b816beb9a83e673cf09b842ea11735fc4e6db
Michael Di Giacomi <michaeldigiacomi@icloud.com>
2020-08-23 10:05:12 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import boto3
+import os
+
+bucketname = "gic-cust-cost-reports"
+bucketprefix = "Raw"
+localpath = "s3"
+
+s3_client = boto3.client('s3', aws_access_key_id='ASIAXNQKKQU5MILNG6US', aws_secret_access_key='Yvp4RgUnrCPG3okslnz8Hocrs68TLxGOLFDXT4YO', aws_session_token='IQoJb3JpZ2luX2VjEJf//////////wEaCXVzLWVhc3QtMSJHMEUCIDcEWDhndv2xzShoJ9y6GQ40YqGsv7eQZ2j5RtqRGQd1AiEA0dJHP93jQlCzKfI5eD0Z6vnWq1TGjwy10iAkPBYzIr4q1QIIgP//////////ARAAGgw1MTAwNDkwMjczODYiDIwqprGMzoKujaLdXSqpAkvOR1s26kwyhk/905Rhc5Ky8GL0y03J61sBCOUzWJba1BiDFw/AWDTxMLRqeFNH+1ofrClcoBYpuuB2E9LO4qqacdLNWgfN9srLGAAx5qfo5l2hI8/drgqMwrscZSZLRv0pnhxOSNEBMr+zm2v+kZ9nIoVVR8b45nsWx8ykFuKknyc6pzcL34jl7b2EGNbA/d44gXgow7QVmfsAzdQnM4qxs3H6piU0aXu8YH5BTsAo/yohsJ5zyChGzwGCUc7qGRVR7BUbcw5bk4Myc7CPenrBjpCq0ZoyNSUin/JigMKPDhNjengJrfp9aKU80dVmPt5zFfdTtk5NMgHY9YPi1GSH8ZNMPy6x2I+njHhzXoLTDetOHrk4NJnjv8eDtWu/oJhiOBNN5C1a5DC5w4b6BTqjAZW0gu0tJXTqQOiF0DBxWxJn2AGG1JKebVFLIzTOc8/RVZ5LxxnHcrO/fN7HeSAvi5Hlgl6qDMu9G28H9abY2irY+ed+rRJL6bng+xsFHGfAUWF1Ps5cY9K4QNmaAukJHB9KuJ44Lp3BjxUjSJAjdkDLl90YgDL3Fm+DzASTYa5+ecFZ8E/QLKUa/s9XU9Nf14T5pysWtxItkj/LEjDx/aZ4pAs=' )
+
+
+def download_dir(prefix, local, bucket, client=s3_client):
+    """
+    params:
+    - prefix: pattern to match in s3
+    - local: local path to folder in which to place files
+    - bucket: s3 bucket with target contents
+    - client: initialized s3 client object
+    """
+    keys = []
+    dirs = []
+    next_token = ''
+    base_kwargs = {
+        'Bucket': bucket,
+        'Prefix': prefix,
+    }
+    while next_token is not None:
+        kwargs = base_kwargs.copy()
+        if next_token != '':
+            kwargs.update({'ContinuationToken': next_token})
+        results = client.list_objects_v2(**kwargs)
+        contents = results.get('Contents')
+        for i in contents:
+            k = i.get('Key')
+            if k[-1] != '/':
+                keys.append(k)
+            else:
+                dirs.append(k)
+        next_token = results.get('NextContinuationToken')
+    for d in dirs:
+        dest_pathname = os.path.join(local, d)
+        if not os.path.exists(os.path.dirname(dest_pathname)):
+            os.makedirs(os.path.dirname(dest_pathname))
+    for k in keys:
+        dest_pathname = os.path.join(local, k)
+        if not os.path.exists(os.path.dirname(dest_pathname)):
+            os.makedirs(os.path.dirname(dest_pathname))
+        print("Starting to Download " + k)
+        client.download_file(bucket, k, dest_pathname)
+        print("Finished Downloading " + k)
+
+
+def getdownloadedfiles(path):
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.csv.gz' in file:
+                files.append(os.path.join(r, file))
+
+    return files
+
+
+#print("Downloading Files")
+#download_dir(bucketprefix, localpath, bucketname)
+print("Listing Files")
+filelist = getdownloadedfiles(localpath)
+print("Script Execution Complete")
+
+for file in filelist:
+    df = pd.read_csv(file, compression='gzip', error_bad_lines=False)
+    for col in df.columns:
+         col2 = col.replace("/", "_")
+         col2 = col2.replace(":", "_")
+         col2 = col2.replace(" ", "2")
+         list1 = list(col2)
+         new_list = []
+         for i in list1:
+             if i.isupper():
+                 i = "_" + i.lower()
+             new_list.append(i)
+         col2 = ''.join(new_list)
+         col2 = col2.replace("__", "_")
+         df.rename(columns={col: col2}, inplace=True)
+    df.to_parquet(file.replace('Raw', 'Processed') + ".parquet")
+
