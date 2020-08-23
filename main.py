461686008862f3fe35ee9a4069cbd350c44e2490
Michael Di Giacomi <michaeldigiacomi@icloud.com>
2020-08-23 15:13:24 -0400

    Removing Prefix

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ parquetfiles = getdownloadedfiles(localpath, '.parquet')
 
 for file in parquetfiles:
     print("Uploading File: " + os.path.basename(file))
-    s3_client.upload_file(file, bucketname, os.path.basename(file))
+    s3_client.upload_file(file, bucketname, file.replace("s3/", ""))
     print("Upload Complete: " + os.path.basename(file))
 
 
